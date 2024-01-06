05f3b9037f964e5377b2003abae97c5773426348
AramZakaryan <aram.h.zakaryan@gmail.com>
2024-01-06 10:48:58 +0400

    Result of Exercise (with children)

--- src/task_3/Task_3.tsx
+++ src/task_3/Task_3.tsx
@@ -4,27 +4,43 @@ import { SlowComponent } from './slowComponent/SlowComponent';
 
 //find the problem and fix it as part of composition optimization, memo, children
 
-export const Task_3 = () => {
+// export const Task_3 = () => {
+//
+//     console.log("Task_3 render")
+//
+//   return (
+//     <div>
+//       <div>Lags when change value</div>
+//       <Input/>
+//       <SlowComponent />
+//     </div>
+//   );
+// };
+//
+//
+// const Input = () => {
+//
+//     console.log("Input render")
+//
+//     const [value, setValue] = useState('');
+//
+//     const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
+//
+//     return <input type="text" value={value} onChange={onChange} />
+// }
 
-    console.log("Task_3 render")
 
-  return (
-    <div>
-      <div>Lags when change value</div>
-      <Input/>
-      <SlowComponent />
-    </div>
-  );
-};
-
-
-const Input = () => {
-
-    console.log("Input render")
+export const Task_3 = ({children}:any) => {
 
     const [value, setValue] = useState('');
 
     const onChange = (event: ChangeEvent<HTMLInputElement>) => setValue(event.currentTarget.value);
 
-    return <input type="text" value={value} onChange={onChange} />
-}
+    return (
+        <div>
+            <div>Lags when change value</div>
+            <input type="text" value={value} onChange={onChange} />
+            {children}
+        </div>
+    );
+};
